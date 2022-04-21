4e22538b5a0e8eb43de4634a9dcd65aa14282f61
flippedcoder <milecia.mcgregor@aol.com>
2022-04-21 10:10:22 -0500

    pipeline updates to add more security tests

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,26 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <MetaTags
+        title="Home"
+        // description="Home description"
+        /* you should un-comment description and add a unique description, 155 characters or less
+      You can look at this documentation for best practices : https://developers.google.com/search/docs/advanced/appearance/good-titles-snippets */
+      />
+
+      <h1>HomePage</h1>
+      <p>
+        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
+      </p>
+      <p>
+        My default route is named <code>home</code>, link to me with `
+        <Link to={routes.home()}>Home</Link>`
+      </p>
+    </>
+  )
+}
+
+export default HomePage
